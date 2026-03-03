3b8cea4b9a5dcd9220285687286080599db5def3
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-03 21:40:02 +0530

    Max Sum of Sub Array

--- src/problems.java
+++ src/problems.java
@@ -121,6 +121,18 @@ public class problems {
         }
         return secondLargest;
     }
+    //Max Sum of Sub Array---------------------->
+    public static int maxSubArray(int[] arr) {
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
     public static void main(String[] args) {
 
 
