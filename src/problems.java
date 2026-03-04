fac474b2c97df1097950cd28483fb52d11c3651c
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-04 23:28:32 +0530

    product Except Self

--- src/problems.java
+++ src/problems.java
@@ -133,6 +133,24 @@ public class problems {
 
         return maxSum;
     }
+    // product Except Self ------------------------>
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * right;
+            right = right * nums[i];
+        }
+
+        return result;
+    }
     public static void main(String[] args) {
 
 
