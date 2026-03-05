6d39882cf8b91bed2c069ea4c9d780623fd4b987
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-05 23:31:08 +0530

    First Non Repeating Element

--- src/problems.java
+++ src/problems.java
@@ -151,6 +151,24 @@ public class problems {
 
         return result;
     }
+    //First Non Repeating Element -------------------------->
+    public static void firstNonRepeating(String s) {
+
+        for (int i = 0; i < s.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println(s.charAt(i));
+                return;
+            }
+        }
+    } /* firstNonRepeating("sshhiivam");*/
     public static void main(String[] args) {
 
 
