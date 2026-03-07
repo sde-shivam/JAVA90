5df7d4c234ed9f2c6910d8ca1bfb4be043b46095
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-07 22:29:33 +0530

    Count Vowels

--- src/problems.java
+++ src/problems.java
@@ -179,6 +179,18 @@ public class problems {
 
     }
     /*reverse("shivam");*/
+    // Count Vowels ----------------------->
+    public static void countVowels(String s) {
+        int count = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                    ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
     public static void main(String[] args) {
 
 
