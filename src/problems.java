3f2662d061ca2f87e017d59d7e7edc62a14e980b
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-09 07:18:28 +0530

    String is palindrome or not

--- src/problems.java
+++ src/problems.java
@@ -205,6 +205,15 @@ public class problems {
 
         return max;
     }
+    // String is palindrome or not ------------------------>
+    public static boolean isPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
 
 
