50f3a66c18d716e4c59d54b901eeccf5d6d8f409
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-11 22:55:11 +0530

    Reverse a String

--- src/problems.java
+++ src/problems.java
@@ -240,10 +240,16 @@ public class problems {
             }
             System.out.println(count);
         }
+        // Reverse a String ------------->
+        public static String reverseStr(String str){
+            StringBuilder sb = new StringBuilder();
+                for (int i = str.length() - 1; i >= 0; i--) {
+                    sb.append(str.charAt(i));
+                }
+                return sb.toString();
+            }
     public static void main(String[] args) {
 
-
     }
-
 }
 
