73162180dc0a12676e9cb825ead5e3f4dc0e8107
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-12 17:52:22 +0530

    Check if a Number is Palindrome

--- src/problems.java
+++ src/problems.java
@@ -248,6 +248,20 @@ public class problems {
                 }
                 return sb.toString();
             }
+    // Check if a Number is Palindrome------------------------>
+    public static boolean isPalindrome(int num) {
+
+        int original = num;
+        int reverse = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        return original == reverse;
+    }
     public static void main(String[] args) {
 
     }
