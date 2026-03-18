5a2e2910a24a911033c30eaa483a29656c0d880a
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-03-18 13:20:24 +0530

    Check Days using switch condition

--- src/problems.java
+++ src/problems.java
@@ -407,8 +407,40 @@ public class problems {
             System.out.print("char is betn n to z");
         }
     }
+    // Check Days using switch condition------------------>
+    public static void checkDays(){
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the Number  :");
+        int day = scn.nextInt();
+
+        switch(day){
+            case 1:
+                System.out.print("Monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+            case 3:
+                System.out.print("Wednesday");
+                break;
+            case 4:
+                System.out.print("Thursday");
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("Saturday");
+                break;
+            case 7:
+                System.out.print("Sunday");
+                break;
+            default:
+                System.out.print("Invalid Number");
+        }
+    }
     public static void main(String[] args) {
-        checkNum();
+        checkDays();
     }
 }
 
