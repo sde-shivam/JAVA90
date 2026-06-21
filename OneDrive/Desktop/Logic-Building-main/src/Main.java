64020b6b1d91b4c60506068ab33aad0bc5882301
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-06-21 21:42:03 +0530

    smaller Numbers Than Current

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -2615,6 +2615,20 @@ public int maximumWealth(int[][] accounts) {
     }
     return max;
 }
+// smaller Numbers Than Current--------------------------->
+public int[] smallerNumbersThanCurrent(int[] nums) {
+    int[] arr = new int[nums.length];
+    for(int i=0;i<nums.length;i++){
+        int count=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[i]>nums[j]){
+                count++;
+            }
+        }
+        arr[i] = count;
+    }
+    return arr;
+}
 public static void main (String args[]) {
     swapCase();
 }
