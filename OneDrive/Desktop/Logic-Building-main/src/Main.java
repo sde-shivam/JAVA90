70b89b1b208171908b88745fd289ca2d04ae4e31
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-06-22 22:01:29 +0530

    Remove element (val) from array and return the same array

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -2629,6 +2629,17 @@ public int[] smallerNumbersThanCurrent(int[] nums) {
     }
     return arr;
 }
+// Remove element (val) from array and return the same array-------------->
+public int removeElement(int[] nums, int val) {
+    int k=0;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]!=val){
+            nums[k]=nums[i];
+            k++;
+        }
+    }
+    return k;
+}
 public static void main (String args[]) {
     swapCase();
 }
