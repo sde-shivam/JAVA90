e40d2096cfb72f63269c828fd6d62319f40567f0
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-06-23 09:12:28 +0530

    Merge two sorted array into the result array than into nums1

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -2640,6 +2640,27 @@ public int removeElement(int[] nums, int val) {
     }
     return k;
 }
+// Merge two sorted array into the result array than into nums1----------->
+public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int i=0,j=0,k=0;
+    int[] result = new int[m+n];
+    while(i < m && j < n){
+        if(nums1[i]<=nums2[j]){
+            result[k++]=nums1[i++];
+        }else{
+            result[k++]=nums2[j++];
+        }
+    }
+    while(i<m){
+        result[k++]=nums1[i++];
+    }
+    while(j<n){
+        result[k++]=nums2[j++];
+    }
+    for(int x = 0; x < m + n; x++) {
+        nums1[x] = result[x];
+    }
+}
 public static void main (String args[]) {
     swapCase();
 }
