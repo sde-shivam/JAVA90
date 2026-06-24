c20d71a54b521c4a289122b9492c236807bc43ed
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-06-24 23:57:37 +0530

    Binary Search For Element in Array

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -2678,6 +2678,22 @@ public int[] sortedSquares(int[] nums) {
     }
     return nums;
 }
+// Binary Search For Element in Array------------>
+public static int binarySearchForEl(int[]nums,int target){
+    int left = 0;
+    int right = nums.length-1;
+    while(left<=right){
+        int mid = (left+right)/2;
+        if(nums[mid]==target){
+            return mid;
+        }else if(nums[mid]<target){
+            left=mid+1;
+        }else{
+            right=mid-1;
+        }
+    }
+    return left;
+}
 public static void main (String args[]) {
     swapCase();
 }
