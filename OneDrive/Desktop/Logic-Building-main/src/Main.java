ab548436d8fb3358e19f13716415a2096a6aa247
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-06-25 14:06:45 +0530

    One plus problem

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -2694,6 +2694,21 @@ public static int binarySearchForEl(int[]nums,int target){
     }
     return left;
 }
+// One plus problem--------------------->
+public int[] plusOne(int[] digits) {
+    for(int i=digits.length-1;i>=0;i--){
+        if(digits[i]!=9){
+            digits[i]+=1;
+            return digits;
+        }else{
+            digits[i]=0;
+        }
+    }
+    int[] ans = new int[digits.length+1];
+    ans[0]=1;
+    return ans;
+
+}
 public static void main (String args[]) {
     swapCase();
 }
