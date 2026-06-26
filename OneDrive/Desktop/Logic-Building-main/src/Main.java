8dcd306cf9d4ac958a80be6894a1e07f4e780f40
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-06-26 22:46:35 +0530

    Count the number actual diff

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -2709,6 +2709,18 @@ public int[] plusOne(int[] digits) {
     return ans;
 
 }
+// Count the number actual diff--------------------->
+public int countKDifference(int[] nums, int k) {
+    int count=0;
+    for(int i=0;i<nums.length-1;i++){
+        for(int j=i+1;j<nums.length;j++){
+            if(Math.abs(nums[i] - nums[j])==k){
+                count++;
+            }
+        }
+    }
+    return count;
+}
 public static void main (String args[]) {
     swapCase();
 }
