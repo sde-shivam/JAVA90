d3194b76425bc4b1a9b1924b12e300dddd0437d5
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-07-03 12:11:43 +0530

    Fizz Buzz

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -2860,6 +2860,22 @@ public int lengthOfLastWord(String s) {
     }
     return lastStr.length();
 }
+// Fizz Buzz problem----------------->
+public List<String> fizzBuzz(int n) {
+    List<String> list = new ArrayList<>();
+    for(int i=1;i<=n;i++){
+        if(i%15==0){
+            list.add("FizzBuzz");
+        }else if(i%3==0){
+            list.add("Fizz");
+        }else if(i%5==0){
+            list.add("Buzz");
+        }else{
+            list.add(Integer.toString(i));
+        }
+    }
+    return list;
+}
 public static void main (String args[]) {
     longestWord("name is shivam singh");
 }
