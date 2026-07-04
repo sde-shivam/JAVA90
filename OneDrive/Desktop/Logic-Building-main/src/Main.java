4bcf71bbbca2bdfff3b586bd931aee6c5e995aaf
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-07-04 22:18:56 +0530

    sum Of Unique elements in array

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -2876,6 +2876,35 @@ public List<String> fizzBuzz(int n) {
     }
     return list;
 }
+//
+public static void evenLengthWord(String str) {
+    int count = 0;
+    String[] newStr = str.split(" ");
+    for (int i = 0; i < newStr.length; i++) {
+        if (newStr[i].length() % 2 == 0) {
+            count++;
+        }
+    }
+}
+
+//sum Of Unique elements in array------------------>
+public int sumOfUnique(int[] nums) {
+    Map<Integer,Integer> map = new HashMap<>();
+    for(int num:nums){
+        if(map.containsKey(num)){
+            map.put(num,map.get(num)+1);
+        }else{
+            map.put(num,1);
+        }
+    }
+    int sum=0;
+    for(int el :map.keySet()){
+        if(map.get(el)==1){
+            sum+=el;
+        }
+    }
+    return sum;
+}
 public static void main (String args[]) {
     longestWord("name is shivam singh");
 }
