96587a7b31cf113f891afd6b9cb2a241fc871ff0
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-07-20 23:00:43 +0530

    Find Max Consecutive One

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -3181,6 +3181,20 @@ public static void removeExtraSpace(String str){
      }
      return org.equals(rev);
  }
+//Find Max Consecutive One----------------------->
+public int findMaxConsecutiveOne(int[] nums) {
+    int count =0;
+    int max =0;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]==1){
+            count++;
+            max = Math.max(count,max);
+        }else{
+            count=0;
+        }
+    }
+    return max;
+}
 public static void main (String args[]) {
     removeExtraSpace("name   is   shivas   singh  he is not goog");
 }
