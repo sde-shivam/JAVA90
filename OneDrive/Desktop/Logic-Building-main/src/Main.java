d6e12a82a1675afe069bd5e43d74dd625fadfbb8
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-07-21 22:17:09 +0530

    Anagram of big string problem

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -3195,6 +3195,29 @@ public int findMaxConsecutiveOne(int[] nums) {
     }
     return max;
 }
+//Anagram of big string problem ---------------->
+public static int[] anagramStr(String big, String small) {
+    big = big.toLowerCase();
+    small = small.toLowerCase();
+
+    char[] smallSorted = small.toCharArray();
+    Arrays.sort(smallSorted);
+
+    ArrayList<Integer> matches = new ArrayList<>();
+    for (int i = 0; i <= big.length() - small.length(); i++) {
+        char[] window = big.substring(i, i + small.length()).toCharArray();
+        Arrays.sort(window);
+
+        if (Arrays.equals(window, smallSorted)) {
+            matches.add(i);
+        }
+    }
+    int[] result = new int[matches.size()];
+    for (int i = 0; i < matches.size(); i++) {
+        result[i] = matches.get(i);
+    }
+    return result;
+}
 public static void main (String args[]) {
     removeExtraSpace("name   is   shivas   singh  he is not goog");
 }
