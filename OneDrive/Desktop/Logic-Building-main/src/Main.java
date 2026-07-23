be04c264f704bc4b6b0c2f4edf8a9fe1c39bbec8
Shivam Singh <shivamasingh.tcsc@gmail.com>
2026-07-23 22:16:59 +0530

    Binary Search method

--- OneDrive/Desktop/Logic-Building-main/src/Main.java
+++ OneDrive/Desktop/Logic-Building-main/src/Main.java
@@ -3227,6 +3227,23 @@ public static int evenSumNum(int num){
     }
     return evenSum(num-1)+sum;
 }
+//Binary Search method --------------------->
+public static int binarySrch(int numbers[], int key) {
+    int start = 0;
+    int end = numbers.length - 1;
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        if (numbers[mid] == key) {
+            return mid;
+        }
+        if (numbers[mid] < key) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
 public static void main (String args[]) {
     removeExtraSpace("name   is   shivas   singh  he is not goog");
 }
